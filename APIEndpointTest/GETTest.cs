f6a6184f4ec326d9b2ff261e125695d331471d33
Ryan Grey <ryan.g.grey@gmail.com>
2016-07-04 22:46:36 +0100

    Add skeleton project for endpoint tests
    This project will test the rest api endpoints

---
+++ APIEndpointTest/GETTest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net.Http;
+
+namespace APIEndpointTest
+{
+    // see example explanation on xUnit.net website:
+    // https://xunit.github.io/docs/getting-started-dotnet-core.html
+    public class GETTest
+    {
+        
+    }
+}
