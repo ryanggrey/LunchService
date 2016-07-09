dbf1cb7a1da5d37d5877ef8cb4acbb2dee5028d6
Ryan Grey <ryan.g.grey@gmail.com>
2016-07-09 14:49:47 +0100

    Add Equals implementation for User
    We also add a hash code implementation

--- LunchService/Models/User/User.cs
+++ LunchService/Models/User/User.cs
@@ -8,5 +8,32 @@ namespace LunchService.Models
         {
             Name = name;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            User user = obj as User;
+            return this.Equals(user);
+        }
+
+        public bool Equals(User user)
+        {
+            if (user == null)
+            {
+                return false;
+            }
+
+            bool isNameEqual = user.Name == this.Name;
+            return isNameEqual;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
     }
 }
