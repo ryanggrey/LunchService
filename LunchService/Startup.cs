6acd722437419a65eaa4c3dbd21cfb5a6cd51c08
Ryan Grey <rgrey@scottlogic.co.uk>
2016-08-01 11:35:29 +0100

    Remove unneeded comments

--- LunchService/Startup.cs
+++ LunchService/Startup.cs
@@ -21,15 +21,12 @@ namespace LunchService
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
             services.AddSingleton<IUserRepository, UserRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder appBuilder, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             Configure(loggerFactory);
